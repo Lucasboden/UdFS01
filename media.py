ee46838172dadbb8d62b57d8ba2e148957546359
LucasBoden <lucashippertt@id.uff.br>
2018-09-26 13:53:41 -0300

    Ajustando formato pep8 e colocando docstring

--- media.py
+++ media.py
@@ -1,10 +1,14 @@
 import webbrowser
+
+
 class Movie:
-    def __init__(self,trailer_youtube_url,cartaz,title,resumo):
+    def __init__(self, trailer_youtube_url, cartaz, title, resumo):
+        """Cria instancia da classe Movie com parametros passados"""
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = cartaz
         self.title = title
         self.resumo = resumo
+
     def mostra_trailer(self):
+        """Funcao que mostra na tela o trailer do filme."""
         webbrowser.open(self.trailer_youtube_url)
-        
